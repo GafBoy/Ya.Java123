602137492fda37866d06976e17a10f1ceafc8f40
Ildar Adullin <iadullin@yandex-team.ru>
2022-09-07 23:32:38 +0300

    Через цикл for Задание:         Необходимо прогнать алгоритм fizz buzz на числах от 1 до n введенного пользователем

--- src/primitive_1.java
+++ src/primitive_1.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 public class primitive_1 {
     public static void main(String[] args) {
-        for (int i = 1; i<= 100; i++) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число до которого хотите проверить кратность: ");
+        int e = in.nextInt();
+        for (int i = 1; i<= e; i++) {
             String result = "";
             if (i % 3 == 0) {
                 result = "fizz ";

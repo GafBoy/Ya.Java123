03b9a9a27d7f5dfe9e56f0dc8027b86efa63b675
Ildar Adullin <iadullin@yandex-team.ru>
2022-09-08 00:17:02 +0300

    Через цикл for Задание:         Необходимо прогнать алгоритм fizz buzz на числах от 1 до каждого четного/нечетного числа до n введенного пользователем

--- src/primitive_1.java
+++ src/primitive_1.java
@@ -5,16 +5,32 @@ public class primitive_1 {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число до которого хотите проверить кратность: ");
         int e = in.nextInt();
-        for (int i = 1; i<= e; i++) {
-            String result = "";
-            if (i % 3 == 0) {
-                result = "fizz ";
+        if (e % 2 == 0) {
+            for (int i = 1; i <= e; i++) {
+                String result = "";
+                if (i % 3 == 0) {
+                    result = "fizz ";
+                }
+                if (i % 5 == 0) {
+                    result = result + "buzz";
+                }
+                System.out.println("Число  " + i + " Результат: " + result);
             }
-            if (i % 5 == 0) {
-                result = result + "buzz";
+        } else {
+            e--;
+            for (int i = 1; i <= e; i++) {
+                String result = "";
+                if (i % 3 == 0) {
+                    result = "fizz ";
+                }
+                if (i % 5 == 0) {
+                    result = result + "buzz";
+                }
+                System.out.println("Число  " + i + " Результат: " + result);
             }
-            System.out.println("Число  " + i + " Результат: " + result);
         }
+
     }
 }
+
 
